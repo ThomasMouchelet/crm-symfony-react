83ec766f642511df75fc451ca0f31df4044fdaa4
ThomasMouchelet <mouchelet.thomas@gmail.com>
2020-07-14 09:21:22 +0200

    count chrono invoice

---
+++ src/Events/InvoiceChronoSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
+
+class InvoiceChronoSubscriber implements EventSubscriberInterface {
+
+    private $security;
+    private $repository;
+
+    public function __construct(Security $security, InvoiceRepository $repository)
+    {
+        $this->security = $security;
+        $this->repository = $repository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setChronoForInvoice', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+    public function setChronoForInvoice(ViewEvent $event){
+        $result = $event->getControllerResult();
+        $method =  $event->getRequest()->getMethod();
+
+        if($result instanceof Invoice && $method === "POST"){
+            $user = $this->security->getUser();
+            $lastChrono = $this->repository->findLastChrono($user);
+            $result->setChrono($lastChrono+1);
+        }
+
+    }
+}
