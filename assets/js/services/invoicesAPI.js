27d231d0e2ffab302e4c9a339c0a2213f0383455
thomas mouchelet <mouchelet.thomas@gmail.com>
2020-07-20 16:31:44 +0200

    private routing && dynamique interaction on login & logout

--- assets/js/services/invoicesAPI.js
+++ assets/js/services/invoicesAPI.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
 function findAll(){
-    return axios.get('http://127.0.0.1:8000/api/invoices')
+    return axios.get('https://127.0.0.1:8000/api/invoices')
             .then(response => response.data["hydra:member"])
 }
 
 function deleteInvoice(id){
-    return axios.delete(`http://127.0.0.1:8000/api/invoices/${id}`)
+    return axios.delete(`https://127.0.0.1:8000/api/invoices/${id}`)
         .then(response => console.log(`La facture numéro ${id} a bien été supprimée`))
 }
 
